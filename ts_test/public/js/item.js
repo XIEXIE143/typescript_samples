226d6bb6c9bbcdf7c4c2dc5e365c22ef61d52b5e
XIEXIE143 <xjy143@outlook.com>
2023-12-05 12:18:56 +0900

    update

--- ts_test/public/js/item.js
+++ ts_test/public/js/item.js
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var itemNames = ["コーヒー", "紅茶", "ほうじ茶"];
 console.log(itemNames);
 var item1 = {
@@ -20,5 +18,58 @@ var item3 = {
 // var items:object[] = [item1, item2, item3];
 var items = [item1, item2, item3];
 var borderPrice = 300;
-var filterItems = items.filter(function (item) { return item.price > borderPrice; });
+var filterItems = items.filter((item) => item.price > borderPrice);
 console.log(filterItems);
+// function
+// function caclculateTotalPrice(price:number, amount:number): number {
+//     var totalPrice = price * amount;
+//     return totalPrice;
+// }
+// anonymous function
+// const caclculateTotalPrice = function(price:number, amount:number): number {
+//     var totalPrice = price * amount;
+//     return totalPrice;
+// }
+// arrow function
+const caclculateTotalPrice = (price, amount) => {
+    var totalPrice = price * amount;
+    return totalPrice;
+};
+var totalPrice = caclculateTotalPrice(item1.price, 5);
+console.log(totalPrice);
+// Union
+var data;
+data = 10;
+data = "Tokyo";
+console.log(data);
+// any
+// function findItem(id: number): any {
+//     return items.find((item) => item.id == id);
+// }
+// var item: Item = findItem(2);
+// Union: Item or undefined
+function findItem(id) {
+    return items.find((item) => item.id == id);
+}
+var item = findItem(3);
+if (item == undefined) {
+    console.log("Not found");
+}
+else {
+    console.log(item);
+}
+// callback
+function purchase(id, amount, callback) {
+    var item = findItem(id);
+    if (item) {
+        callback(item, amount);
+    }
+}
+function payment(item, amount) {
+    //TODO payment
+    var totalPrice = caclculateTotalPrice(item.price, amount);
+    console.log("Payment:" + totalPrice);
+    return true;
+}
+purchase(1, 5, payment);
+export {};
--- ts_test/public/js/type.js
+++ ts_test/public/js/type.js
@@ -1,6 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// interface User {
+// export type User = {
 //     id: number;
 //     name: {
 //         first: string;
@@ -9,3 +7,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //     email: string;
 //     isActive: boolean;
 // }
+export {};

3bd58abd0887addb7f134eb4fc828ecc6c3bc41b
XIEXIE143 <xjy143@outlook.com>
2023-12-05 10:07:07 +0900

    update

--- ts_test/item.ts
+++ ts_test/item.ts
@@ -1,23 +1,29 @@
 var itemNames:string[] = ["コーヒー", "紅茶", "ほうじ茶"];
 console.log(itemNames)
 
-var item1 = {
+type Item = {
+    id: number;
+    name: string;
+    price: number;
+}
+
+var item1: Item = {
     id: 1,
     name: "コーヒー",
     price: 350,
 }
-var item2 = {
+var item2: Item = {
     id: 2,
     name: "紅茶",
     price: 400,
 }
-var item3 = {
+var item3: Item = {
     id: 3,
     name: "ほうじ茶",
     price: 300,
 }
 // var items:object[] = [item1, item2, item3];
 var items = [item1, item2, item3];
-const borderPrice = 300
+var borderPrice = 300;
 var filterItems = items.filter((item) => item.price > borderPrice )
 console.log(filterItems)
